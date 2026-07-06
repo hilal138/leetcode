7a87016b57a4a507806eeedc832caee5b8b26915
Hilal Ahmad Sheikh <sheikhhilal138@gmail.com>
2026-07-06 18:37:34 +0530

    Time: 105 ms (7.29%), Space: 40.8 MB (99.28%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,45 +1,22 @@
 class Solution {
 public:
-    vector<list<pair<int,int>>> graph;
+
     vector<int> time;
 
-    void add_edge(int src, int dest, int weight){
-        graph[src].push_back({dest, weight}); 
-    }
-
     int networkDelayTime(vector<vector<int>>& arr, int n, int src) {
-
-        graph.resize(n + 1);
-        
-        for(int i=0;i<arr.size();i++){
-            add_edge(arr[i][0], arr[i][1], arr[i][2]);
-        }
-
         time.resize(n+1 ,INT_MAX);
-        
         time[src] = 0; 
-        
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        
-        pq.push({0, src});
-        
-        while(!pq.empty()){
-            int distance = pq.top().first;
-            int node = pq.top().second;
-            pq.pop();
-            
-            if(distance > time[node]) continue;
-            
-            for(auto  edge : graph[node]){
-                int neighbor = edge.first;
-                int weight = edge.second;
-                
-                if(time[node] + weight < time[neighbor]){
-                    time[neighbor] = time[node] + weight;
-                    pq.push({time[neighbor], neighbor});
-                }
+
+        for(int i=1;i<=n-1;i++){
+            for(int j=0;j<arr.size();j++){
+                int u = arr[j][0];
+                int v = arr[j][1];
+                int wt = arr[j][2];
+                if(time[u]!=INT_MAX and time[u]+wt < time[v] ) time[v]=time[u]+wt;
             }
         }
+        
+        
         int Max=INT_MIN;
         for(int i=1;i<=n;i++){
             if(time[i]==INT_MAX) return -1;
