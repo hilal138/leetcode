7768d7ab1d4ecc221196430a777534eb0b944a9f
Hilal Ahmad Sheikh <sheikhhilal138@gmail.com>
2026-07-07 20:52:20 +0530

    Time: 0 ms (100%), Space: 21.8 MB (70.71%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        int sum=0;
+        for(auto ele : nums) sum+=ele;
+        int mxsum=n*(n+1)/2;
+
+        return mxsum-sum;
+    }
+};
