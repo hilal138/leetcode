ee8524fee281bf2fdaa57db21ec08724bb2b1622
Hilal Ahmad Sheikh <sheikhhilal138@gmail.com>
2026-07-08 11:03:43 +0530

    Time: 0 ms (100%), Space: 23.8 MB (55.94%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,22 +1,13 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int n = nums.size();
-        vector<int>helper;
-        helper=nums;
-        for(int i=0;i<n;i++){
-            nums[i]=0;
-        }
-        int i=0;
-        int j=0;
-        while(i<n){
-            if(helper[i]==0) i++;
-            else{
-                nums[j]=helper[i];
+        int i = 0;  // Position for the next non-zero element
+
+        for (int j = 0; j < nums.size(); j++) {
+            if (nums[j] != 0) {
+                swap(nums[i], nums[j]);
                 i++;
-                j++;
             }
         }
-        return;
     }
 };
