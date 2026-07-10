48afb61b6374cd649addb894ec62596778fa98ad
Hilal Ahmad Sheikh <sheikhhilal138@gmail.com>
2026-07-10 10:34:31 +0530

    Time: 0 ms (100%), Space: 13.2 MB (60.9%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -4,13 +4,11 @@ public:
         int n =arr.size();
         int lo=0;
         int hgh=n-1;
-        int idx=-1;
         int missing=0;
         while(lo<=hgh){
             int mid = lo +(hgh-lo)/2;
             missing=arr[mid]-(mid+1);
             if(missing>=k){
-                idx=mid;
                 hgh=mid-1;
             }
             else lo=mid+1;
