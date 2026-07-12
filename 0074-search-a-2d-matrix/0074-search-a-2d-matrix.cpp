4a52d8976f2a0911c0764722d5f82aa904d20e39
Hilal Ahmad Sheikh <sheikhhilal138@gmail.com>
2026-07-12 21:08:48 +0530

    Time: 0 ms (100%), Space: 13.4 MB (7.41%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& arr, int target) {
+        int m = arr.size();
+        int n =arr[0].size();
+        int i=0;
+        int j=n-1;
+        while(i<m and j >=0){
+            if(arr[i][j]==target){
+                return true;
+                break;
+            }
+            else if (arr[i][j]>target) j--;
+            else i++;
+        }
+    return false;
+    }
+};
