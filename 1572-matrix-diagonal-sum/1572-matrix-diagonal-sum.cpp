9db44b9b94ddc2d0e9a755db36ec6082e1a43b20
Hilal Ahmad Sheikh <sheikhhilal138@gmail.com>
2026-07-13 11:54:48 +0530

    Time: 0 ms (100%), Space: 14.8 MB (83.7%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
@@ -7,14 +7,9 @@ public:
 
         for(int i=0;i<n;i++){
             sum+=arr[i][i];
+            sum+=arr[i][n-1-i];
         }
-        int i=0;
-        int j=n-1;
-        while(i<n and j >=0){
-            sum+=arr[i][j];
-            i++;
-            j--;
-        }
+
         if(n%2==0) return sum;
         else return sum-arr[n/2][n/2];
     }
