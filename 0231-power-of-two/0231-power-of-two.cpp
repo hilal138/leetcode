79f8428ce7a31e2a684b3461f7a36666ef45fa8f
Hilal Ahmad Sheikh <sheikhhilal138@gmail.com>
2026-07-25 21:17:53 +0530

    Time: 0 ms (100%), Space: 7.7 MB (82.52%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,10 +1,7 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        int m=n/2;
-        for(int i=0;i<=30;i++){
-            if(1<<i==n) return true;
-        }
-        return false;
+        if(n==0) return false;
+        return n>0 and  ((n&n-1)==0);
     }
 };
