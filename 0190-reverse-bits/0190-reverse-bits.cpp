f106455743d07dd93e8ec91a2bcb6b72e99669da
Hilal Ahmad Sheikh <sheikhhilal138@gmail.com>
2026-07-26 11:42:29 +0530

    Time: 0 ms (100%), Space: 8.3 MB (14.49%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int reverseBits(int n) {
+        int ans=0;
+        for(int i=0;i<32;i++){
+            ans<<=1;
+            int lastbit= n&1;
+            ans=ans | lastbit;
+            
+            n>>=1;
+        }
+        return ans;
+    }
+};
